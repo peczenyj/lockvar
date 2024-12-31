9a4eda64b5069029f6f36f2f14d27f1fe3a988fb
Tiago Peczenyj <tpeczenyj@weborama.com>
2024-12-31 16:40:56 +0100

    add more examples

--- semaphore/semaphore_test.go
+++ semaphore/semaphore_test.go
@@ -3,6 +3,7 @@ package semaphore_test
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/peczenyj/go-protected/semaphore"
 )
@@ -16,27 +17,43 @@ func ExampleNew() {
 		*i++
 	})
 
+	protectedVar.TryUse(1, func(i *int) {
+		*i++
+	})
+
 	protectedVar.Do(ctx, func(i *int) {
 		fmt.Println("got i=", *i)
 	})
 
 	// Output:
-	// got i= 1
+	// got i= 2
 }
 
 func ExampleProtectedVar_TryUse() {
 	protectedVar := semaphore.New(new(int), 1)
 
-	ctx := context.Background()
-
-	protectedVar.Use(ctx, 1, func(i *int) {
-		*i++
+	success := protectedVar.TryUse(10, func(i *int) {
+		fmt.Println("got i=", *i) // will never be called
 	})
 
-	protectedVar.TryUse(1, func(i *int) {
-		fmt.Println("got i=", *i)
-	})
+	fmt.Printf("TryUse returns %t", success)
 
 	// Output:
-	// got i= 1
+	// TryUse returns false
+}
+
+func ExampleProtectedVar_Use() {
+	protectedVar := semaphore.New(new(int), 1)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
+	defer cancel()
+
+	err := protectedVar.Use(ctx, 10, func(i *int) {
+		fmt.Println("got i=", *i) // will never be called
+	})
+
+	fmt.Printf("Use returns %v", err)
+
+	// Output:
+	// Use returns context deadline exceeded
 }
